0a1525cebb7d981d971e116a0002260df3292ba8
Hunterb9101 <hunterb9101@gmail.com>
2017-01-05 20:01:44 -0700

    This should have been added
    Go figure...

--- src/entity/StormTrooper.java
+++ src/entity/StormTrooper.java
@@ -1,10 +1,30 @@
 package entity;
 
-public class StormTrooper extends Entity {
+import java.awt.Image;
 
-	public StormTrooper() {
+import battleField.Registry;
+
+public class Stormtrooper extends Entity {
+	Animation idleImg = new Animation(new Image[]{Registry.loadImage("stormtrooper/StormtrooperIdle.png")});
+	Animation walkImg = new Animation(new Image[]{Registry.loadImage("stormtrooper/walk/StormtrooperWalk0.png"),
+			Registry.loadImage("stormtrooper/walk/StormtrooperWalk1.png"),
+			Registry.loadImage("stormtrooper/walk/StormtrooperWalk2.png"),
+			Registry.loadImage("stormtrooper/walk/StormtrooperWalk3.png"),
+			Registry.loadImage("stormtrooper/walk/StormtrooperWalk4.png")});
+	Animation prepImg = new Animation(new Image[]{Registry.loadImage("stormtrooper/StormtrooperPrep.png")});
+	Animation fireImg = new Animation(new Image[]{Registry.loadImage("stormtrooper/StormtrooperFire.png")});
+	Animation reloadImg = new Animation(new Image[]{Registry.loadImage("stormtrooper/StormtrooperPrep.png")});
+	
+	public Stormtrooper() {
 		super("Stormtrooper");
-		
+		setImgSources(idleImg,walkImg,prepImg,fireImg,reloadImg);
+		width = 35;
+		height = 49;
+		speed = 2;
+		range = 120;
+		hitPoints = 15;
+		attack = 30;
+		energyCost = 1;
 	}
 
 }
