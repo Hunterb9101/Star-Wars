fd547df09246956596ffffa8976e1ffe61bbb360
Hunterb9101 <hunterb9101@gmail.com>
2017-03-17 14:15:45 -0600

    Icosahedron Subdivide
    Does not work yet

--- src/planets/Icosphere.java
+++ src/planets/Icosphere.java
@@ -56,6 +56,37 @@ public class Icosphere extends Object3D{
         faces.add(new Triangle(vertices.get(2), vertices.get(4), vertices.get(11)));
         faces.add(new Triangle(vertices.get(6), vertices.get(2), vertices.get(10)));
         faces.add(new Triangle(vertices.get(8), vertices.get(6), vertices.get(7)));
-        faces.add(new Triangle(vertices.get(9), vertices.get(8), vertices.get(1)));     
+        faces.add(new Triangle(vertices.get(9), vertices.get(8), vertices.get(1))); 
+        
+        for (int i = 0; i < recursionLevel; i++) {
+        	ArrayList<Face2D> faces2 = new ArrayList<Face2D>();
+          for(int f = 0; f<faces.size(); f++){
+              // replace triangle by 4 triangles
+              Point3D a = createMiddlePoint(faces.get(i).vertices[0], faces.get(i).vertices[1]);
+              Point3D b = createMiddlePoint(faces.get(i).vertices[1], faces.get(i).vertices[2]);
+              Point3D c = createMiddlePoint(faces.get(i).vertices[2], faces.get(i).vertices[0]);
+
+              faces2.add(new Triangle(faces.get(i).vertices[0], a, c));
+              faces2.add(new Triangle(faces.get(i).vertices[1], b, a));
+              faces2.add(new Triangle(faces.get(i).vertices[2], c, b));
+              faces2.add(new Triangle(a, b, c));
+          }
+          faces.addAll(faces2);
+          System.out.println(faces.size());
+        }
+    }
+    
+    private Point3D createMiddlePoint(Point3D p1, Point3D p2){
+        // not in cache, calculate it
+        Point3D middle = new Point3D(
+            (int)((p1.x + p2.x) / 2.0), 
+            (int)((p1.y + p2.y) / 2.0), 
+            (int)((p1.z + p2.z) / 2.0));
+
+        // add vertex makes sure point is on unit sphere
+        double length = Math.sqrt(Math.pow(middle.x,2)+Math.pow(middle.y,2)+Math.pow(middle.z,2));
+        middle = new Point3D((int)(size*(double)middle.x/(double)length)+x, (int)(size*(double)middle.y/(double)length) + y, (int)(size*(double)middle.z/(double)length));
+        vertices.add(middle); 
+        return middle;
     }
 }
